5847982e107333fabddfd2400cb21bbfce8ca265
trankhuonghuy000 <huy.khuong.000@gmail.com>
2026-03-25 16:23:41 +0700

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import "./App.css"
+import {Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Homepage.jsx"
+import Checkout from "./pages/Checkout.jsx"
+import Auth from "./pages/Auth.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 function App() {
 
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div className="app">
+      <Navbar />  
+      <Routes>
+        <Route path = "/" element = {< Home/>} />
+        <Route path = "/auth"  element = {<Auth/>} />
+        <Route path = "/checkout"  element = {<Checkout />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import getproducts from "../data/product";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+  const products = getproducts();
+  return (
+    <div className="page">
+      <div className="home-hero">
+        <h1 className="home-title">Welcome to ShopHub</h1>
+        <p className="home-subtitle">
+          Discover amazing products at unbeatable prices!
+        </p>
+      </div>
+      <div className="container">
+        <h2 className="page-title">Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
